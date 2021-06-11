c629a8edb613d99523ec8d97cceaa42f94b3e258
Scott Corbat <scorbat@riis.com>
2021-06-11 07:57:43 -0400

    no longer show tab menu if there is only one day option

--- ETA Detroit/View/StopsView.swift
+++ ETA Detroit/View/StopsView.swift
@@ -14,19 +14,34 @@ struct StopsView: View {
     let route: Route
     let color: Color
     
+    var existingDays: [String] {
+        return dataService.getDaysOfCurrentStops()
+    }
+    
     var body: some View {
         VStack {
             Text("Stops for \(route.name)")
                 .font(.title)
 
-            TabView {
-                ForEach(dataService.getDaysOfCurrentStops(), id: \.self) { day in
-                    List(dataService.stops[day]!) { stop in
-                        StopCellView(dataService: dataService, stop: stop, color: color)
+            //if there are multiple options then display those
+            if existingDays.count > 1 {
+                TabView {
+                    ForEach(existingDays, id: \.self) { day in
+                        List(dataService.stops[day]!) { stop in
+                            StopCellView(dataService: dataService, stop: stop, color: color)
+                        }
+                        .tabItem {
+                            Label(day, systemImage: "1.circle")
+                        }
                     }
-                    .tabItem {
-                        Label(day, systemImage: "1.circle")
-                    }
+                }
+            //otherwise don't display the option
+            } else if existingDays.count == 1 {
+                List(dataService.stops[existingDays[0]]!) { stop in
+                    StopCellView(dataService: dataService, stop: stop, color: color)
+                }
+                .tabItem {
+                    Label(existingDays[0], systemImage: "1.circle")
                 }
             }
         }
